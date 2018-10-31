73caa70403adf1435ca9ca6012ce44c0bb7993ac
EliJArmstrong <41320998+EliJArmstrong@users.noreply.github.com>
2018-10-31 12:58:09 -0700

    Update FileSizes/FileSizesForm.cs

--- FileSizes/FileSizesForm.cs
+++ FileSizes/FileSizesForm.cs
@@ -1,27 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <author>Eli Armstrong</author>
+// <remarks>I pledge my word of honor that I have abided
+// by the CSN Academic Integrity Policy while completing
+// this assignment.</remarks>
+// <file>FileSizesForm.cs</file>
+// <date>2018-10-26</date>
+// <summary>This class is a GUI that creates a combobox of directories. 
+// The current directory's files and their sizes are displayed in a text 
+// box.</summary> 
+// <remarks>Time taken to develop, write, test and debug
+// solution. About 2.5 hours. </remarks>
+
+using System;
 using System.IO;
 using System.Windows.Forms;
 
+// ----------------------------------------------------------------------------
+// The Name Space for FileSizes.
+// ----------------------------------------------------------------------------
 namespace FileSizes
 {
+    /// -----------------------------------------------------------------------
+    /// <summary>The FileSizesForm partial class with base of Form.</summary>
+    /// -----------------------------------------------------------------------
     public partial class FileSizesForm : Form
     {
-
-
-
+        /// -------------------------------------------------------------------
+        /// <summary>Constructor</summary>
+        ///
+        /// <remarks>
+        /// Calls the InitializeComponent method and updates the UI.
+        /// </remarks>
+        /// -------------------------------------------------------------------
         public FileSizesForm()
         {
             InitializeComponent();
             UpdateUI();
         }
 
+        /// -------------------------------------------------------------------
+        /// <summary>
+        /// This method gets called when an option is selected.
+        /// Then the current directory is updated and the UI is updated.
+        /// </summary>
+        /// <param name="sender">The caller of the method.</param>
+        /// <param name="e">The event choices that are offered.</param>
+        /// -------------------------------------------------------------------
         private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             try
@@ -42,7 +65,12 @@ namespace FileSizes
             }
         }
 
-
+        /// -------------------------------------------------------------------
+        /// <summary>
+        /// This method updates the UI by updating the directory 
+        /// combobox and files and sizes of the current directory.
+        /// </summary>
+        /// -------------------------------------------------------------------
         private void UpdateUI()
         {
             fileInfoBox.Clear();
@@ -52,7 +80,7 @@ namespace FileSizes
             directoryComboBox.Items.AddRange(Directory.GetDirectories(Directory.GetCurrentDirectory()));
             foreach (var fileName in Directory.GetFiles(Directory.GetCurrentDirectory()))
             {
-                FileInfo fileInfo = new FileInfo(fileName);
+                FileInfo fileInfo = new FileInfo(fileName); // Gets file info.
                 string filename = fileInfo.Name;
 
                 if (fileInfo.Name.Length > 30)
